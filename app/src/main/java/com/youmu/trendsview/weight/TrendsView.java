7c67fff38cbb4a0bfc6a8ced507dc6ecf81e44ed
youzehong <youmu178@sina.com>
2015-11-19 18:00:43 +0800

    Initial commit

---
+++ app/src/main/java/com/youmu/trendsview/weight/TrendsView.java
@@ -0,0 +1,108 @@
+package com.youmu.trendsview.weight;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.youmu.trendsview.R;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by youzehong on 15/11/15.
+ */
+public class TrendsView extends View {
+
+    private ArrayList<Item> mItemList = new ArrayList<>();
+
+    public TrendsView(Context context) {
+        this(context, null);
+    }
+
+    public TrendsView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TrendsView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void initData() {
+        for (int i = 0; i < 20; i++) {
+            Item item = new Item(getWidth(), getHeight());
+            mItemList.add(item);
+        }
+    }
+
+    public class Item {
+        private float mLeft;
+        private float mTop;
+        private float mOpt;
+        private Paint mPaint;
+        private int mScreenWidth;
+        private int mScreenHeight;
+        Random mRandom = new Random();
+        private Bitmap mMoveBitmap;
+
+        public Item(int width, int height) {
+            this.mScreenWidth = width;
+            this.mScreenHeight = height;
+            init();
+            if (mMoveBitmap == null) {
+                mMoveBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.leaf);
+            }
+        }
+
+        private void init() {
+            mPaint = new Paint();
+            mPaint.setAntiAlias(true);
+            mLeft = mRandom.nextInt(mScreenWidth);
+            mTop = mRandom.nextInt(mScreenHeight);
+            mOpt = mRandom.nextInt(80);
+        }
+
+        private void onMove() {
+            mLeft += mLeft * mOpt;
+            mTop += mTop * mOpt;
+            if (mTop > mScreenHeight) {
+                init();
+            }
+        }
+
+        private void onDraw(Canvas canvas) {
+            canvas.drawBitmap(mMoveBitmap, mLeft, mTop, mPaint);
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        postDelayed(mRunnable, 30);
+        for (Item item : mItemList) {
+            item.onDraw(canvas);
+        }
+    }
+
+    Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            initData();
+            // 移动
+            for (Item item : mItemList) {
+                item.onMove();
+            }
+            postInvalidate();
+        }
+    };
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        removeCallbacks(mRunnable);
+    }
+}
